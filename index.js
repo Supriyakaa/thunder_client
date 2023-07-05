1a4d2cfd91b29df67cdcf261b16bbd10926a1a06
Supriya Pathak <supriya.pathak@tvsmotor.com>
2023-07-05 13:30:05 +0530

    1st day

---
+++ index.js
@@ -0,0 +1,15 @@
+ const  express = require("express");
+ const  app = express()
+ const Port = 8000;
+
+
+ app.get("/", (req,res) => {
+
+    res.send("helllooooooo")
+ })
+
+app.listen(Port,() => {
+     console.log(`listning to ${Port}`)
+
+})
+//  app.listen(8000)
